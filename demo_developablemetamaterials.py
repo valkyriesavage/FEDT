7184cf35bd6c4c82856dda0c2b913d3e5774392b
Valkyrie Savage <vasa@di.ku.dk>
2024-08-08 10:31:08 +0200

    attempting to fix devmeta but it none of the expts work at the moment

--- demo_developablemetamaterials.py
+++ demo_developablemetamaterials.py
@@ -3,6 +3,7 @@ import math
 from numpy import arange
 
 from instruction import instruction
+from iterators import Series, Parallel, include_last
 from measurement import Measurements
 from fabricate import RealWorldObject
 from decorator import fedt_experiment
@@ -18,21 +19,20 @@ def test_density_and_materials():
 
     # test impact of ruffle density and materials on compression
     results = Measurements.empty()
-    for ruffle_design in ['default', 'wide', 'dense']:
+    for ruffle_design in Parallel(['default', 'wide', 'dense']):
         ruffle_file = SvgEditor.design(ruffle_design)
-        for load_direction in ['X','Y']: # ? I'm not sure if this should be the outer loop or the inner one, were the same objects used?
-            for material in ['flipchart', 'office paper', 'plastic']:
-                instruction(f"ensure {material} is loaded in the lasercutter")
+        for material in Parallel(['flipchart', 'office paper', 'plastic']):
+            for load_direction in Parallel(['X','Y']): # unclear if different objects were used for X and Y or not (series? parallel?)
                 fabbed_object = Laser.fab(ruffle_file, material=material)
                 fabbed_object = Human.post_process(fabbed_object, "assemble matched ruffles")
                 loads = [0,10,20,50,100,200]
                 results += Calipers.measure_size(fabbed_object,'height at rest in {load_direction}')
                 fabbed_object = Human.post_process(fabbed_object, "apply acrylic plate in {load_direction}")
-                current_load = 0
-                while Human.is_reasonable(fabbed_object) and current_load < len(loads):
-                    results += Calipers.measure_size(fabbed_object,f'height at load {loads[current_load]} in {load_direction}')
-                    current_load += 1
-                    fabbed_object = Human.post_process(fabbed_object, f"apply load {loads[current_load]} in {load_direction}")
+                for current_load in Series(loads):
+                    results += Calipers.measure_size(fabbed_object,f'height at load {current_load} in {load_direction}')
+                    if not Human.is_reasonable(fabbed_object):
+                        break
+                    fabbed_object = Human.post_process(fabbed_object, f"apply load {current_load} in {load_direction}")
                 # then relax it
                 fabbed_object = Human.post_process(fabbed_object, "remove all loads")
                 results += Calipers.measure_size(fabbed_object,'height at unload in {load_direction}')
@@ -42,20 +42,21 @@ def test_density_and_materials():
 @fedt_experiment
 def design_versus_stiffness():
     results = Measurements.empty()
-    ruffle_file = LineFile("default_ruffle.svg")
-    for load_direction in ['X','Y']: # ? I'm not sure if this should be the outer loop or the inner one
-        instruction("ensure 160g paper is loaded in the lasercutter")
-        for tab_condition in ['cut tabs', 'engrave + tape']:
-            fabbed_object = Laser.fab(ruffle_file, material="160g paper", tab_condition=tab_condition)
+    tab_conditions_files = [("cut tabs", LineFile("default_ruffle_cuttabs.svg")),
+                            ("no tabs + tape", LineFile("default_ruffle_notabs.svg"))]
+    loads = [0,10,20,50,100,200]
+
+    for tab_condition, ruffle_file in Parallel(tab_conditions_files):
+        for load_direction in Parallel(['X','Y']): # unclear if different objects were used for X and Y or not
+            fabbed_object = Laser.fab(ruffle_file, material="160g paper")
             fabbed_object = Human.post_process(fabbed_object, f"assemble matched ruffles with {tab_condition}")
-            loads = [0,10,20,50,100,200]
             results += Calipers.measure_size(fabbed_object,'height at rest in {load_direction}')
             fabbed_object = Human.post_process(fabbed_object, "apply acrylic plate in {load_direction}")
-            current_load = 0
-            while Human.is_reasonable(fabbed_object) and current_load < len(loads):
-                results += Calipers.measure_size(fabbed_object,f'height at load {loads[current_load]} in {load_direction}')
-                current_load += 1
-                fabbed_object = Human.post_process(fabbed_object, f"apply load {loads[current_load]} in {load_direction}")
+            for current_load in Series(loads):
+                results += Calipers.measure_size(fabbed_object,f'height at load {current_load} in {load_direction}')
+                if not Human.is_reasonable(fabbed_object):
+                    break
+                fabbed_object = Human.post_process(fabbed_object, f"apply load {current_load} in {load_direction}")
                 # how did they determine how much load to add?
             # then relax it
             fabbed_object = Human.post_process(fabbed_object, "remove all loads")
@@ -64,4 +65,4 @@ def design_versus_stiffness():
     summarize(results.get_data())
 
 if __name__ == "__main__":
-    print(test_density_and_materials())
+    print(design_versus_stiffness())
