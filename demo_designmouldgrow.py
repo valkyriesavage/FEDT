2ca9f4746c57823c1dae67412f7562ac2a1c6831
Valkyrie Savage <vasa@di.ku.dk>
2024-08-08 10:20:20 +0200

    dmg loops

--- demo_designmouldgrow.py
+++ demo_designmouldgrow.py
@@ -1,12 +1,11 @@
 from numpy import arange
 
 from instruction import instruction
+from iterators import Series, Parallel, include_last
 from measurement import Measurements
 from fabricate import RealWorldObject
 from decorator import fedt_experiment
 from lib import *
-
-include_last = .001
 
 def compare(dataset1, dataset2):
     return "Hmmmm... I think these two are pretty same-same??"
@@ -29,44 +28,29 @@ class CustomModellingTool:
 def geometric_features():
     shrinkage_results = Measurements.empty()
     scanning_results = Measurements.empty()
-    for geometry_file in ['ramps.stl', 'circular.stl', 'patterns.stl']:
+    comparison_results = []
+
+    geometries = [VolumeFile(x) for x in ['ramps.stl', 'circular.stl', 'patterns.stl']]
+
+    for geometry_file in Parallel(geometries):
         mould = Printer.slice_and_print(geometry_file)
-        for myco_material in ['30% coffee inclusions', 'no inclusions']:
+        for myco_material in Series(['30% coffee inclusions', 'no inclusions']):
             fabbed_object = Human.mould_mycomaterial(mould, myco_material)
             Environment.wait_up_to_time_single(fabbed_object, num_weeks=1)
-            # this loop should unroll somehow so all the grow takes place at once...
-            # although each mould is used for 2 different, sequential grows :thinking_face:
             shrinkage_results += Calipers.measure_size(fabbed_object, "important dimension")
-            if geometry_file != 'ramps.stl':
-                scanning_results += Scanner.scan(fabbed_object)
-        if geometry_file == 'circular.stl':
-            oneoff_object = Human.mould_mycomaterial(mould, 'no inclusions')
-            oneoff_object = Human.post_process(oneoff_object, 'glycerine treatment')
-            shrinkage_results += Calipers.measure_size(oneoff_object, "important dimension")
-            scanning_results += Scanner.scan(oneoff_object)
+            if geometry_file.stl_location != 'ramps.stl':
+                scan = Scanner.scan(fabbed_object)
+                scanning_results += scan
+            if geometry_file.stl_location == 'circular.stl':
+                oneoff_object = Human.mould_mycomaterial(mould, 'no inclusions')
+                oneoff_object = Human.post_process(oneoff_object, 'glycerine treatment')
+                shrinkage_results += Calipers.measure_size(oneoff_object, "important dimension")
+                scanning_results += Scanner.scan(oneoff_object)
+    
+    # several derivative measurement operations are ignored here for simplicity; e.g., the extraction of 2D profiles from the scans
 
     shrinkage_results = shrinkage_results.get_data()
     scanning_results = scanning_results.get_data()
-
-    for result in scanning_results:
-        real = None
-        digital = None
-        if result.geometry_file == 'circular': # TODO how to do this?
-            for angle in arange(7.5, 82.5+include_last, 7.5):
-                real = StlEditor.extract_profile(result, angle)
-                digital = StlEditor.extract_profile(result.geometry_file, angle)
-                result.comparison = compare(real, digital)
-        if result.geometry_file == 'patterns':
-            for offset in arange(0, 1.5+include_last, 0.1):
-                real = StlEditor.extract_profile(result, offset)
-                digital = StlEditor.extract_profile(result.geometry_file, offset)
-                result.comparison = compare(real, digital)
-        if Human.is_reasonable(result) and real is not None and digital is not None:
-            # only process the ones that seem reasonable (they skipped the non-inclusion patterns one)
-            result.error = StlEditor.uniform_sample_error(real, digital)
-            if result.geometry_file == 'patterns':
-                result.convexity_error = StlEditor.sample_convex(real, digital)
-                result.concavity_error = StlEditor.sample_concave(real, digital)
 			
     summarize(shrinkage_results)
     summarize(scanning_results)
@@ -80,14 +64,14 @@ def mechanical_and_shrinkage_features():
     shrinkage_results = Measurements.empty()
     mechanical_results = Measurements.empty()
     mould = Printer.slice_and_print(scaled_mould)
-    for myco_material in ['30% coffee inclusions', 'no inclusions']:
-        for repetition in range(4):
+    for myco_material in Series(['30% coffee inclusions', 'no inclusions']): # I can't tell if this was series or parallel
+        for repetition in Series(range(4)):
             fabbed_object = Human.mould_mycomaterial(mould, myco_material)
             Environment.wait_up_to_time_single(fabbed_object, num_weeks=1)
             shrinkage_results += Calipers.measure_size(fabbed_object, "x-axis")
             shrinkage_results += Calipers.measure_size(fabbed_object, "y-axis")
             shrinkage_results += Calipers.measure_size(fabbed_object, "z-axis")
-            for repetition_mechanical in range(5):
+            for repetition_mechanical in Series(range(5)):
                 for depth in arange(0,5,.5):
                     instruction("ensure fabbed object is appropriately arranged on testing stand")
                     fabbed_object = Human.post_process(fabbed_object, "compress the object to " + str(depth)) # not clear who/what does the compressing?
@@ -104,7 +88,7 @@ def test_software_tool():
     results = Measurements.empty()
     mould = Printer.slice_and_print(software_generated_mould)
     myco_material = "30% coffee inclusions"
-    for repetition in range(3):
+    for repetition in Parallel(range(3)):
         fabbed_object = Human.mould_mycomaterial(mould, myco_material)
         Environment.wait_up_to_time_single(fabbed_object, num_weeks=1)
         measurement_points = range(0,90,45)
